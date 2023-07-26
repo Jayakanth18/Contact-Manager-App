2b5ab65e3bc7fc6ea2702d66ccbf03f1712eb6e5
Jayakanth <jayakanth1812@gmail.com>
2023-07-26 11:48:22 +0530

    modified code

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 const dbConnect = async () => {
   try {
-    const connect = await mongoose.connect(
-      process.env.DB ||
-        "mongodb+srv://admin:admin@jk-cluster.amks6oa.mongodb.net/mycontacts-backend?retryWrites=true&w=majority"
-    );
+    const connect = await mongoose.connect(process.env.DB_CONNECTION_STRING);
     console.log(
       "success in db connection",
       connect.connection.host,
