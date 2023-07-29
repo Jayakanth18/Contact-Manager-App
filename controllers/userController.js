160480c2d08ec12bcbc6e17231225f027975c214
Jayakanth <jayakanth1812@gmail.com>
2023-07-29 17:59:15 +0530

    used jwt for user authorization

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 //post
@@ -17,28 +18,53 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User already registered");
   }
   const hashedPassword = await bcrypt.hash(password, 10);
-//   console.log("hashed password:", hashedPassword);
-  const user =await User.create({
+  //   console.log("hashed password:", hashedPassword);
+  const user = await User.create({
     username,
     email,
     password: hashedPassword,
   });
   console.log(`User created`);
-  if(user){
-    res.status(201).json({_id: user.id, email: user.email})
-  } else{
-    res.status(400)
-    throw new Error("User data is not valid")
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
   }
   res.json({ message: "Register the user" });
 });
+
 //post
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "login user" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory like email and password");
+  }
+  const user = await User.findOne({ email });
+  // comparing password with hashed password in the db.
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.JWT_ACCESS_TOKEN_SECERT_KEY,
+      { expiresIn: "1m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("email or password not valid");
+  }
 });
+
 //get
 const currentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Current user information" });
+  res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
--- routes/user.js
+++ routes/user.js
@@ -6,10 +6,11 @@ const {
   currentUser,
 } = require("../controllers/userController");
 
+const validateToken = require("../middleware/validateTokenHandler");
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
 
-router.get("/current", currentUser);
+router.get("/current", validateToken, currentUser);
 
 module.exports = router;
